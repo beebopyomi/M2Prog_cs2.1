076f1a4e6dcfc991250fd8103127788e6d7d61b1
Jakub <jakubszymanzki@gmail.com>
2025-12-09 13:11:07 +0100

    help bro idk wat ik aan het doen ben dit werkt niet eens

---
+++ 03_var/03/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _03;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.run();
+        }
+
+        class QuizVraag
+    {
+        internal string vraag;
+        internal string antwoord;
+        
+    
+        internal QuizVraag(string vraag, string antwoord)
+        {
+            this.vraag = vraag;
+            this.antwoord = antwoord;
+        }
+    }
+
+
+
+    internal void run()
+    {
+        QuizVraag quizVraag = new QuizVraag("welke jaar vielen de twin towers?", "2001");
+        Quiz quiz = new Quiz(10);
+    }
+    class Quiz
+    {
+    internal QuizVraag[] vragen;
+    internal QuizVraagAntwoord[] Ingevuldeantwoorden;
+    }
+
+    internal Quiz()
+    {
+        vragen = new QuizVraag[3];
+    }
+    internal QuizVraagenAntwoord(WuizVraag vraag)
+    {
+        vraag = vraag;
+        goed = false;
+    }
+    
+    
+}
